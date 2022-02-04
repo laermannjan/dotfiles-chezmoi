917dee788606368c85c325a1b3cbc997b1cf76fb
Jan Laermann <laermannjan@gmail.com>
2022-02-04 19:45:40 +0100

    add asdf

---
+++ private_dot_config/private_fish/conf.d/asdf.fish
@@ -0,0 +1,3 @@
+# asdf is a univeral runtime version manager (https://asdf-vm.com)
+# asdf scripts need to be sourced after you have set your $PATH and after you have sourced your framework (oh-my-zsh etc).
+source "(brew --prefix asdf)/libexec/asdf.fish"
--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -12,18 +12,3 @@ end
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
-
-
-# Make Homebrew available
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-
-
-
-
-# Haskell stuff
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
-set -gx PATH $HOME/.cabal/bin /home/jan/.ghcup/bin $PATH # ghcup-env
-
-# asdf is a univeral runtime version manager (https://asdf-vm.com)
-source /home/linuxbrew/.linuxbrew/opt/asdf/libexec/asdf.fish
